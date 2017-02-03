8bb5fc3ae37e713e6c46b7b2d6e8e9f66ac1c69a
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-03 12:52:31 -0800

    further study complete

--- tests.py
+++ tests.py
@@ -22,6 +22,7 @@ class PartyTests(unittest.TestCase):
         # # db.session.close()
         # db.drop_all()
 
+
     def test_homepage(self):
         result = self.client.get("/")
         self.assertIn("board games, rainbows, and ice cream sundaes", result.data)
@@ -55,7 +56,9 @@ class PartyTestsDatabase(unittest.TestCase):
 
         self.client = app.test_client()
         app.config['TESTING'] = True
-
+        with self.client.session_transaction() as sess:
+            sess['RSVP'] = True
+        # self.client.session_transaction()['RSVP'] = True
         # Connect to test database (uncomment when testing database)
         connect_to_db(app, "postgresql:///testdb")
 
